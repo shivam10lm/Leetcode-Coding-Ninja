666b3cec229a9b5ccbcab802b5c5e6e6a7ece6e7
Shivam Gupta <51778763+shivam10lm@users.noreply.github.com>
2026-08-26 14:18:13 -0600

    Create groupAnagram.java

---
+++ patterns/groupAnagram.java
@@ -0,0 +1,29 @@
+//T.C - 0(m*n) S.C - 0(mn)
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        
+        Map<String, List<String>> ans = new HashMap<>();
+
+        for(String s : strs){
+            int[] count = new int[26];
+
+            for(char c : s.toCharArray()){
+                count[c - 'a']++;
+            }
+
+            StringBuilder sb = new StringBuilder();
+
+            for(int num : count){
+                sb.append(num).append("#");
+            }
+
+            String key = sb.toString();
+            if(!ans.containsKey(key)){
+                ans.put(key, new ArrayList<>());
+            }
+
+            ans.get(key).add(s);
+        }
+        return new ArrayList<>(ans.values());
+    }
+}
