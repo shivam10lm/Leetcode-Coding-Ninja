980d9ad750f70756b13229a71ab365064c9270b0
Shivam Gupta <51778763+shivam10lm@users.noreply.github.com>
2026-08-29 16:15:02 -0600

    Update caesarCipher.java

--- patterns/caesarCipher.java
+++ patterns/caesarCipher.java
@@ -1,25 +1,32 @@
+import java.util.*;
+
 public class Main {
 
-public static String caesarCipher(String text, int shift) {
+    public static String caesarCipher(String text, int shift){
 
-    text = text.toLowerCase();
-    StringBuilder result = new StringBuilder();
+        StringBuilder result = new StringBuilder();
 
-    shift = shift % 26;
+        shift = shift % 26;
 
-    for (int i = 0; i < text.length(); i++) {
-        char ch = text.charAt(i);
+        for(int i = 0; i < text.length(); i++){
 
-        if (ch >= 'a' && ch <= 'z') {
-            char shifted = (char) ('a' + (ch - 'a' + shift) % 26);
-            result.append(shifted);
-        } else {
-            result.append(ch);
+            char ch = text.charAt(i);
+
+            if(ch >= 'a' && ch <= 'z'){
+              char shifted = (char)('a' + ((ch - 'a') + shift) % 26);
+              result.append(shifted);
+            }else if(ch >= 'A' && ch <= 'Z'){
+              char shifted = (char)('A' + ((ch - 'A') + shift) % 26);
+              result.append(shifted);
+            }else{
+              result.append(ch);
+            }
+
         }
-    }
 
-    return result.toString();
-}
+        return result.toString();
+
+    }
 
     public static void main(String[] args) {
       System.out.println(caesarCipher("HELLO", 3));
